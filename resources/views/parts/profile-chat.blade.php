d3eb1c807aadcf132343bcc654f088f666b0102c
Ahsan Hanif <ahsan@timeahead.co>
2023-09-16 02:17:49 +0500

    Management plan design frontend

--- resources/views/parts/profile-chat.blade.php
+++ resources/views/parts/profile-chat.blade.php
@@ -30,6 +30,11 @@
                         <div class="m_btm_right_icons">
                             <ul>
                                 <li>
+                                    <a title="Management Plan" class="m_btn_right_icon_each m_btn_management_plan active" data-id="management-plan">
+                                        <i class="fas fa-list-ul"></i>
+                                    </a>
+                                </li>
+                                <li>
                                     <a title="Add contact" class="m_btn_right_icon_each m_btn_add_contact active" data-id="add-contact">
                                         <i class="fas fa-user-plus"></i>
                                     </a>
@@ -69,6 +74,137 @@
                     </div>
 
                     <div class="edit_elem_bottom">
+                        <div class="each_dash_section instant_hide" data-value="management-plan">
+                            <div>
+                                <div class="mt-10">
+                                    <div class="border-b border-gray-200">
+                                        <nav class="-mb-px flex" aria-label="Tabs">
+                                            <a href="#" class="border-indigo-500 text-indigo-600 w-1/3 border-b-2 py-4 px-1 text-center text-sm font-medium">Stage one</a>
+                                            <a href="#" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 w-1/3 border-b-2 py-4 px-1 text-center text-sm font-medium" aria-current="page">Stage one</a>
+                                            <a href="#" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 w-1/3 border-b-2 py-4 px-1 text-center text-sm font-medium">Stage three</a>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="my-12">
+                                <nav class="flex" aria-label="Breadcrumb">
+                                    <ol role="list" class="flex space-x-4 rounded-md bg-white px-6 shadow">
+                                        <li class="flex">
+                                            <div class="flex items-center">
+                                                <a href="#" class="text-gray-400 hover:text-gray-500">
+                                                <svg class="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
+                                                </svg>
+                                                <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Stage one</a>
+                                                </a>
+                                            </div>
+                                        </li>
+                                        <li class="flex">
+                                            <div class="flex items-center">
+                                                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
+                                                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                                                </svg>
+                                                <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Website design</a>
+                                            </div>
+                                        </li>
+                                    </ol>
+                                </nav>
+                            </div>
+                            <div class="mt-12">
+                                <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6">
+                                    <li class="col-span-1 flex rounded-md shadow-sm cursor-pointer">
+                                        <div class="flex w-16 flex-shrink-0 items-center justify-center bg-pink-600 rounded-l-md text-sm font-medium text-white">01</div>
+                                        <div class="relative flex flex-1 items-center justify-between md:truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                                            <div class="flex-1 md:truncate px-4 py-2 text-sm hover:bg-gray-50">
+                                                <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Website design</a>
+                                                <p class="text-gray-500">Create a webpage, include bio, thubnail and background image</p>
+                                            </div>
+                                            <span class="absolute right-2 top-2 md:right-6 md:top-4 text-gray-400 hover:text-gray-700">
+                                                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li class="col-span-1 flex rounded-md shadow-sm cursor-pointer">
+                                        <div class="flex w-16 flex-shrink-0 items-center justify-center bg-purple-600 rounded-l-md text-sm font-medium text-white">02</div>
+                                        <div class="relative flex flex-1 items-center justify-between md:truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                                            <div class="flex-1 md:truncate px-4 py-2 text-sm">
+                                                <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Update agreements</a>
+                                                <p class="text-gray-500">Update any existying aggreements with your network</p>
+                                            </div>
+                                            <span class="absolute right-2 top-2 md:right-6 md:top-4 text-gray-400 hover:text-gray-700">
+                                                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li class="col-span-1 flex rounded-md shadow-sm cursor-pointer">
+                                        <div class="flex w-16 flex-shrink-0 items-center justify-center bg-yellow-500 rounded-l-md text-sm font-medium text-white">03</div>
+                                        <div class="flex flex-1 items-center justify-between md:truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                                            <div class="flex-1 md:truncate px-4 py-2 text-sm hover:bg-gray-50">
+                                                <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Album / Song</a>
+                                                <p class="text-gray-500">Describe what album / song you want to make</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="col-span-1 flex rounded-md shadow-sm cursor-pointer">
+                                        <div class="flex w-16 flex-shrink-0 items-center justify-center bg-green-500 rounded-l-md text-sm font-medium text-white">04</div>
+                                        <div class="flex flex-1 items-center justify-between md:truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                                            <div class="flex-1 md:truncate px-4 py-2 text-sm hover:bg-gray-50">
+                                                <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Industry contacts</a>
+                                                <p class="text-gray-500">Use industry contacts to find professionals to complete your project</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="mt-12">
+                                <div class="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
+                                    <div class="px-4 sm:px-0">
+                                        <h2 class="text-base font-semibold leading-7 text-gray-900">Platform advice</h2>
+                                        <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+                                    </div>
+                                    <div class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 p-6">
+                                        <p class="text-sm leading-6 text-gray-600">
+                                            Write a few sentences about yourself.<br>
+                                            Write a few sentences about yourself. Write a few sentences about yourself. Write a few sentences about yourself.
+                                            Write a few sentences about yourself. Write a few sentences about yourself. Write a few sentences about yourself.
+                                            Write a few sentences about yourself. Write a few sentences about yourself.
+                                            Write a few sentences about yourself.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3 mb-5">
+                                    <div class="px-4 sm:px-0">
+                                        <h2 class="text-base font-semibold leading-7 text-gray-900">Notes / Storyboard</h2>
+                                        <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+                                    </div>
+                                    <div class="md:col-span-2">
+                                        <div class="col-span-full flex flex-col">
+                                            <div class="mt-2">
+                                                <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-none p-2 sm:text-sm sm:leading-6"></textarea>
+                                            </div>
+                                            <button type="submit" class="rounded-md ml-auto mt-3 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3 mb-5">
+                                    <div class="px-4 sm:px-0">
+                                        <h2 class="text-base font-semibold leading-7 text-gray-900">Button</h2>
+                                        <p class="mt-1 text-sm leading-6 text-gray-600"></p>
+                                    </div>
+                                    <div class="md:col-span-2">
+                                        <div class="col-span-full">
+                                            <button type="submit" class="rounded bg-indigo-50 px-2 py-2 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100 w-full ring-1 ring-inset ring-indigo-600">
+                                                Go to website design
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="each_dash_section instant_hide" data-value="add-contact">
                             <div class="pro_music_search pro_music_info no_border">
                                 <div class="pro_note">
@@ -822,7 +958,7 @@
         </div>
     </div>
 </div>
-<link rel="stylesheet" href="{{asset('css/profile.chat.css?v=1.9')}}">
+<link rel="stylesheet" href="{{asset('css/profile.chat.css?v=1.10')}}">
 <link rel="stylesheet" href="{{asset('select2/select2.min.css')}}"></link>
 <script src="{{ asset('select2/select2.min.js') }}"></script>
 <script>
@@ -958,7 +1094,7 @@
         });
     });
 
-    $('.m_btn_add_contact, .m_btn_diary, .m_btn_industry-contacts, .m_btn_transactions, .m_btn_questionnaires').click(function(e){
+    $('.m_btn_management_plan, .m_btn_add_contact, .m_btn_diary, .m_btn_industry-contacts, .m_btn_transactions, .m_btn_questionnaires').click(function(e){
 
         var id = $(this).attr('data-id');
         $('.each_dash_section:not(.each_dash_section[data-value="'+id+'"])').addClass('instant_hide');
