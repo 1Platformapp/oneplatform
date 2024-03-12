656b5907d9d24071b577e07e666976248cd2db0b
Ahsan Hanif <ahsan@timeahead.co>
2024-03-12 16:10:23 +0500

    Updated app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css?v=3.53') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=3.64') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
