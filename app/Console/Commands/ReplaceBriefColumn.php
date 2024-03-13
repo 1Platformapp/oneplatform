d15ec3d62401b5f8775dce2e74eb7db337035b6b
Quwat <Quwat@TimeAhead.co>
2024-03-13 17:12:27 +0500

    Command to Update Brief in Questionaire

---
+++ app/Console/Commands/ReplaceBriefColumn.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AgentQuestionnaire;
+use App\Models\CreativeBrief;
+use Illuminate\Console\Command;
+
+class ReplaceBriefColumn extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'replace:skill';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $creatives = CreativeBrief::all();
+        foreach($creatives as $creative) {
+            $this->line('Replacing '. $creative->title);
+            $questionaires = AgentQuestionnaire::where('brief_title', $creative->title)->get();
+            foreach($questionaires as $questionaire) {
+                $questionaire->fill(['brief_id' => $creative->id]);
+                $questionaire->save();
+            }
+        }
+        logger('Shifting skills into skill id in agent questionaires Command');
+    }
+}
