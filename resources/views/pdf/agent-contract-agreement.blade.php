230216da1ca7c31adb605f5e8f3e3c93ec7f1834
Quwat <Quwat@TimeAhead.co>
2024-03-30 14:31:57 +0500

    Agreement Contract PDF UI fixed

--- resources/views/pdf/agent-contract-agreement.blade.php
+++ resources/views/pdf/agent-contract-agreement.blade.php
@@ -6,12 +6,12 @@
             table { width: 100% !important; }
         </style>
     </head>
-    <body @if(!isset($view))style="width:1000px;" @endif>
+    <body @if(!isset($view))style="width:100%;" @endif>
         <table align="center" style="width:100%;" cellpadding="0" cellspacing="0" >
             <tr>
                 <td style="width:100%;vertical-align:top;">
-                    <table align="center" style="width:100%; padding-left: 12px; padding-right: 17px;" cellpadding="0" cellspacing="0" >
-                        <tr>
+                    <table align="center" style="width:100%; padding-left: 12px;  padding-right: 12px;" cellpadding="0" cellspacing="0" >
+                        <!-- <tr>
                             <td style="height: 8px;width:55%;vertical-align:top;"></td>
                             <td style="width:30%;vertical-align:top;"></td>
                             <td style="width:15%;vertical-align:top;"></td>
@@ -20,7 +20,7 @@
                             <td style="color:#000;font-size: 24px; width:100%;vertical-align:bottom;">
                                 1Platform Network Agency
                             </td>
-                        </tr>
+                        </tr> -->
                         <tr>
                             <td style="height: 30px;width:55%;vertical-align:top;"></td>
                             <td style="width:30%;vertical-align:top;"></td>
@@ -86,13 +86,27 @@
                             <td style="width:30%;vertical-align:top;"></td>
                             <td style="width:15%;vertical-align:top;"></td>
                         </tr>
-                        <tr>
-                            <td style="color:#888;font-size: 14px;width:100%;vertical-align:bottom;">
+                        <tr style="max-width:100%;">
+                            <td colspan="3" style="font-size: 16px;color: #818181;margin: 10px 0;width:120%;vertical-align:bottom;">
                                 {!! $contractDetails.'<br /><br />'.$contract->custom_terms !!}
                             </td>
-                            <td style="width:30%;vertical-align:top;"></td>
-                            <td style="width:15%;vertical-align:top;"></td>
                         </tr>
+                        @if($contract->contract->advisory_notes)
+                        <tr>
+                            <td style="height: 15px"></td>
+                        </tr>
+                        <tr>
+                            <td style="color:#000; font-size: 17px;  width:100%;vertical-align:bottom;">ADVISORY NOTES</td>
+                        </tr>
+                        <tr>
+                            <td style="height: 15px"></td>
+                        </tr>
+                        <tr style="max-width:100%;">
+                            <td colspan="3" style="font-size: 16px;color: #818181;margin: 10px 0;vertical-align:bottom;">
+                                {!! $contract->contract->advisory_notes !!}
+                            </td>
+                        </tr>
+                        @endif
                         @if(count($signatures))
                         <tr>
                             <td style="height: 15px;width:55%;vertical-align:top;"></td>
@@ -100,11 +114,12 @@
                             <td style="width:15%;vertical-align:top;"></td>
                         </tr>
                         <tr style="max-width:100%;">
-                            <td style="width:60%;vertical-align:top;">
+                            <td style="width:49%;vertical-align:top;">
                                 <img style="max-width:150px; max-height: 100px;" src="{{public_path('signatures/'.$signatures['agent'])}}"><br /><br />
                                 {{$legalNames['agent']}}
                             </td>
-                            <td style="width:60%;vertical-align:top;">
+                            <td style="width:2%;vertical-align:top;"></td>
+                            <td style="width:49%;vertical-align:top;">
                                 <img style="max-width:150px; max-height: 100px;" src="{{public_path('signatures/'.$signatures['artist'])}}"><br /><br />
                                 {{$legalNames['artist']}}
                             </td>
@@ -115,11 +130,27 @@
                             <td style="width:15%;vertical-align:top;"></td>
                         </tr>
                         <tr style="max-width:100%;">
-                            <td style="width:60%;vertical-align:top;">
+                            <td style="width:49%;vertical-align:top;">
                                 {{date('d-m-Y', strtotime($contract->created_at))}}
                             </td>
-                            <td style="width:60%;vertical-align:top;">
+                            <td style="width:2%;vertical-align:top;"></td>
+                            <td style="width:49%;vertical-align:top;">
                                 {{date('d-m-Y')}}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height: 45px;"></td>
+                        </tr>
+                        <tr style="max-width:100%;">
+                            <td colspan="3">
+                                <p style="font-size: 16px;color: #818181;margin: 10px 0;">
+                                    <span style="color: red;">Disclaimer:</span> 1Platform is not responsible for any agreements made between users on the platform. 
+                                    Our website serves as a platform for users to buy, sell, and collaborate. 
+                                    We do not take responsibility for any disputes or legal issues arising from these interactions. 
+                                    Users are advised to exercise caution and diligence when engaging with others on the platform. 
+                                    By using our services, you agree that 1Platform cannot be held liable for any such disputes, 
+                                    and you waive any right to take legal action against the platform.
+                                </p>
                             </td>
                         </tr>
                         @endif
