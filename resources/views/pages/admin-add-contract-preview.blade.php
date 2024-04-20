b7da0564031c891a74587c69fe8f2e55552239fc
Quwat <Quwat@TimeAhead.co>
2024-04-20 15:20:52 +0500

    Client Doc:Contract preview limiting content

--- resources/views/pages/admin-add-contract-preview.blade.php
+++ resources/views/pages/admin-add-contract-preview.blade.php
@@ -45,7 +45,7 @@
     $details = $body;
 @endphp
 
-<div class="contact_details_outer relative">
+<div class="relative contact_details_outer">
     <img class="absolute top-1/2 z-50 left-1/2 tranform -translate-x-[50%] -translate-y-[50%] -rotate-45" src="{{asset('images/preview.png')}}" alt="">
     <div class="contact_details_inner">
         @if(Auth::check())
@@ -60,8 +60,8 @@
             </div>
             <div class="contact_section_body">
                 <form class="flex flex-col" id="signature-form" action="#" method="POST">
-                    <div class="element_container mt-8">
-                    {!! nl2br($details) !!}
+                    <div class="mt-8 element_container">
+                    {!! nl2br(substr($details, 0, 3000)) !!}
 
                     </div>
                     <div class="my-12">
@@ -74,28 +74,28 @@
                             <textarea disabled placeholder="Enter terms on top of above..." rows="4" class="px-2 outline-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"></textarea>
                         </div>
                     </div>
-                    <div class="flex flex-col lg:flex-row lg:items-end lg:justify-between mt-24 mb-12 gap-20">
+                    <div class="flex flex-col gap-20 mt-24 mb-12 lg:flex-row lg:items-end lg:justify-between">
                         <div class="flex flex-col w-full lg:w-1/2">
-                            <button type="button" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-4 mb-2 text-center hover:border-gray-400">
-                                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                            <button type="button" class="relative block w-full px-12 py-4 mb-2 text-center border-2 border-gray-300 border-dashed rounded-lg hover:border-gray-400">
+                                <svg class="w-12 h-12 mx-auto text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
                                 </svg>
-                                <span class="mt-2 block text-sm text-gray-400">Add signature or</span>
-                                <label for="signature-file" class="inline-flex items-center rounded-md bg-gray-400 px-2 cursor-pointer mt-2 py-1 text-sm text-white shadow-sm hover:bg-gray-500">
+                                <span class="block mt-2 text-sm text-gray-400">Add signature or</span>
+                                <label for="signature-file" class="inline-flex items-center px-2 py-1 mt-2 text-sm text-white bg-gray-400 rounded-md shadow-sm cursor-pointer hover:bg-gray-500">
                                     Upload signature file
                                 </label>
                                 <input type="file" accept="image/*" class="absolute top-[-100px] left-[-100px] w-0 h-0 inset-0 opacity-0 -z-50 pointer-events-none" />
                             </button>
-                            <div id="signature-result" class="instant_hide flex justify-center">
+                            <div id="signature-result" class="flex justify-center instant_hide">
                                 <img id="signature-image" class="mb-2" src="">
                                 <span class="cursor-pointer"><i class="fa fa-times-circle"></i></span>
                             </div>
-                            <div class="border-t border-solid pt-1 border-black text-center font-medium">
-                                <input id="legal-name" class="border-b border-solid pt-1 border-black text-center font-medium" type="text" placeholder="Enter your legal name" />
+                            <div class="pt-1 font-medium text-center border-t border-black border-solid">
+                                <input id="legal-name" class="pt-1 font-medium text-center border-b border-black border-solid" type="text" placeholder="Enter your legal name" />
                             </div>
                         </div>
                         <div class="flex flex-col w-full lg:w-1/2">
-                            <div class="border-t border-solid pt-1 border-black text-center font-medium">Artist</div>
+                            <div class="pt-1 font-medium text-center border-t border-black border-solid">Artist</div>
                         </div>
                     </div>
                     <div>
