d9576471b3f5fb053b1d42d49f0ebd4d19f3444a
Ahsan Hanif <ahsan@timeahead.co>
2024-05-14 19:29:48 +0500

    Personal chat for buyer only users--2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css?v=3.81') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=3.82') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
