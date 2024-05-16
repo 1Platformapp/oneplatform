6c272c141a5c84f8a6eba63e4aa7daa928b54b4a
Ahsan Hanif <ahsan@timeahead.co>
2024-05-16 16:11:23 +0500

    Adjusting chat are height for mobile

--- resources/views/parts/profile-chat.blade.php
+++ resources/views/parts/profile-chat.blade.php
@@ -571,7 +571,7 @@
         </div>
     </div>
 </div>
-<link rel="stylesheet" href="{{asset('css/profile.chat.css?v=1.16')}}">
+<link rel="stylesheet" href="{{asset('css/profile.chat.css?v=1.17')}}">
 <link rel="stylesheet" href="{{asset('select2/select2.min.css')}}"></link>
 <script src="{{ asset('select2/select2.min.js') }}"></script>
 <style>
