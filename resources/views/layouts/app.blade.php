80b0c5140b880eb4b738c84e2dff01ae2bf66c92
Ahsan Hanif <ahsan@timeahead.co>
2024-06-04 14:01:38 +0500

    Supporter work--2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css?v=3.85') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=3.86') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
